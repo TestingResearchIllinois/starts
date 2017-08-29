079b0297fdfde71fb348f43c332b6b7787ea4ffb
Owolabi Legunsen <legunse2@illinois.edu>
2017-08-29 09:57:33 -0500

    we no longer need to install yasgl on our own

--- scripts/prepare.sh
+++
@@ -1,6 +0,0 @@
-# This script installs yasgl graph library.
-
-CWD=$( cd $( dirname $0 ) && pwd )
-
-mvn install:install-file -Dfile=${CWD}/yasgl-1.0-SNAPSHOT.jar -DpomFile=${CWD}/yasgl-1.0-SNAPSHOT.pom \
-     -DgroupId=edu.illinois -DartifactId=yasgl -Dversion=1.0-SNAPSHOT -Dpackaging=jar
