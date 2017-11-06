1f530c476abefe995858d66d5de33701f973eeaf
Yujin Zhang <yujinz2@linux-a2.ews.illinois.edu>
2017-11-06 13:32:55 -0600

    read nonAffected from disk for graph update
    write nonAffected to disk at level FINER in RunMojo
    
    minor fix
    
    write nonAffected to disk at level FINER in RunMojo
    
    revert diffmojo

--- starts-plugin/src/main/java/edu/illinois/starts/jdeps/UpdateMojo.java
+++ starts-plugin/src/main/java/edu/illinois/starts/jdeps/UpdateMojo.java
@@ -4,6 +4,10 @@
 
 package edu.illinois.starts.jdeps;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashSet;
@@ -11,6 +15,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.logging.Level;
 
+import edu.illinois.starts.helpers.Writer;
 import edu.illinois.starts.util.Logger;
 import edu.illinois.starts.util.Pair;
 import org.apache.maven.plugin.MojoExecutionException;
@@ -20,25 +25,41 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 
 /**
- * Update test dependencies on disk.
+ * Update class and test dependencies.
  */
 @Mojo(name = "update", requiresDirectInvocation = true, requiresDependencyResolution = ResolutionScope.TEST)
 public class UpdateMojo extends DiffMojo {
     public void execute() throws MojoExecutionException {
-        Logger.getGlobal().setLoggingLevel(Level.parse(loggingLevel));
-
-        Set<String> changed = new HashSet<>();
-        Set<String> nonAffected = new HashSet<>();
-        Pair<Set<String>, Set<String>> data = computeChangeData();
-        String extraText = "";
-        if (data != null) {
-            nonAffected = data.getKey();
-            changed = data.getValue();
+        long start = System.currentTimeMillis();
+        Set<String> nonAffected = null;
+        String inFilename = getArtifactsDir() + File.separator + "non-affected-tests";
+        File inFile = new File(inFilename);
+        if (inFile.isFile()) {
+            nonAffected = readFromFile(inFile, getArtifactsDir());
+            long end = System.currentTimeMillis();
+            Logger.getGlobal().log(Level.FINE, "[PROFILE] readFromFile " + inFilename + " : "
+                    + Writer.millsToSeconds(end - start));
         } else {
-            extraText = " (no RTS artifacts; likely the first run)";
+            Pair<Set<String>, Set<String>> data = computeChangeData();
+            nonAffected = data == null ? new HashSet<String>() : data.getKey();
+            long end = System.currentTimeMillis();
+            Logger.getGlobal().log(Level.FINE, "[PROFILE] computeChangeData(): "
+                            + Writer.millsToSeconds(end - start));
         }
-        printResult(changed, "ChangedClasses" + extraText);
-        //updateDiffChecksums is always true for this module
+        //updateDiffChecksums is always true for this mojo
         updateForNextRun(nonAffected);
+    }
+
+    public Set<String> readFromFile(File inFile, String artifactsDir) {
+        Set<String> result = new HashSet<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(inFile))) {
+            String className = null;
+            while ((className = reader.readLine()) != null) {
+                result.add(className);
+            }
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return result;
     }
 }
