aac4628a485d4a3588cbb0f9a8313e98bea5b934
Jeff Hong <hong66@illinois.edu>
2017-11-16 13:09:25 -0600

    Update AgentLoader.java

--- starts-core/src/main/java/edu/illinois/starts/maven/AgentLoader.java
+++ starts-core/src/main/java/edu/illinois/starts/maven/AgentLoader.java
@@ -36,12 +36,10 @@ public final class AgentLoader {
         if (toolsJarFile == null) {
             return false;
         }
-
         Class<?> vc = loadVirtualMachine(new URL[]{toolsJarFile});
         if (vc == null) {
             return false;
         }
-
         attachAgent(vc, aju);
         return true;
     }
