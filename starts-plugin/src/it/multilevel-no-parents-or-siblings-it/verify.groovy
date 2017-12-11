b73edb5d6c5639c8e59fd428891e396886760212
Jeff Hong <hong66@illinois.edu>
2017-12-11 16:52:42 -0600

    fixes multilevel-no-parents-or-siblings-it

--- starts-plugin/src/it/multilevel-no-parents-or-siblings-it/verify.groovy
+++ starts-plugin/src/it/multilevel-no-parents-or-siblings-it/verify.groovy
@@ -4,6 +4,7 @@
 
 import edu.illinois.starts.jdeps.VerifyUtil;
 
+targetDir = new File(basedir, "target");
 firstRun = new File(basedir, "first-run.txt");
 verifyUtil = new VerifyUtil(new File(basedir, "build.log"));
 
@@ -14,4 +15,7 @@ if (!firstRun.exists()) {
     verifyUtil.assertCorrectlyAffected("2");
     verifyUtil.deleteFile(firstRun);
     verifyUtil.deleteFile(new File(basedir, ".starts/deps.zlc"));
+    file = new File(basedir, "src/main/java/inter/Child.java");
+    VerifyUtil.replaceAllInFileStatic(file, "Set g", "Set<Integer> g");
+    verifyUtil.deleteFolder(targetDir);
 }
