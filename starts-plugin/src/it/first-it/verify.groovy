c2697697de0edad17f4ab43c745efe3ed31d1dff
Jeff Hong <hong66@illinois.edu>
2017-12-11 16:38:53 -0600

    adds extra steps in verify.groovy to remove certain artifacts for subsequent integration-test runs. adds method to remove directories to verifyutil.java

--- starts-plugin/src/it/first-it/verify.groovy
+++ starts-plugin/src/it/first-it/verify.groovy
@@ -4,6 +4,7 @@
 
 import edu.illinois.starts.jdeps.VerifyUtil;
 
+targetDir = new File(basedir, "target");
 firstRun = new File(basedir, "first-run.txt");
 verifyUtil = new VerifyUtil(new File(basedir, "build.log"));
 verifyUtil.assertCorrectlyAffected("1");
@@ -13,4 +14,7 @@ if (!firstRun.exists()) {
 } else {
     verifyUtil.deleteFile(firstRun);
     verifyUtil.deleteFile(new File(basedir, ".starts/deps.zlc"));
+    file = new File(basedir, "src/main/java/first/First.java");
+    VerifyUtil.replaceAllInFileStatic(file, "Set g", "Set<Integer> g");
+    verifyUtil.deleteFolder(targetDir);
 }
--- starts-plugin/src/test/java/edu/illinois/starts/jdeps/VerifyUtil.java
+++ starts-plugin/src/test/java/edu/illinois/starts/jdeps/VerifyUtil.java
@@ -7,7 +7,9 @@ package edu.illinois.starts.jdeps;
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.List;
 
 import edu.illinois.starts.constants.StartsConstants;
@@ -15,7 +17,7 @@ import edu.illinois.starts.constants.StartsConstants;
 import org.junit.Assert;
 
 /**
- * Util methods for scripts that verify results of integration tests.
+ * Util methods for scripts that verify results of integration tests and reset src code to original state.
  */
 public class VerifyUtil implements StartsConstants {
     private List<String> buildLog;
@@ -33,9 +35,33 @@ public class VerifyUtil implements StartsConstants {
         }
     }
 
+    public static void replaceAllInFileStatic(File file, String before, String after) throws IOException {
+        Path path = file.toPath();
+        Charset charset = StandardCharsets.UTF_8;
+        String content = new String(Files.readAllBytes(path), charset);
+        content = content.replaceAll(before, after);
+        Files.write(path, content.getBytes(charset));
+    }
+
     public void deleteFile(File file) {
         if (file.exists()) {
             file.delete();
         }
+    }
+
+    public void deleteFolder(File directory) {
+        if (directory.exists()) {
+            File[] files = directory.listFiles();
+            if (null != files) {
+                for (int i = 0; i < files.length; i++) {
+                    if (files[i].isDirectory()) {
+                        deleteFolder(files[i]);
+                    } else {
+                        files[i].delete();
+                    }
+                }
+            }
+        }
+        directory.delete();
     }
 }
