7e1f775eca4cc26ee2b5e8483f9a5b263938eee9
Jeff Hong <hong66@illinois.edu>
2017-12-15 09:30:30 -0600

    missed one thing in verify.groovy, now all ITs are consistent and follow good coding style

--- starts-plugin/src/it/first-it/verify.groovy
+++ starts-plugin/src/it/first-it/verify.groovy
@@ -11,12 +11,13 @@ verifyUtil.assertCorrectlyAffected("1")
 if (!firstRun.exists()) {
     firstRun.createNewFile()
 } else {
-    changedFile = new File(basedir, "src/main/java/first/First.java")
-    VerifyUtil.replaceAllInFileStatic(changedFile, "Set g", "Set<Integer> g")
     resetIT()
 }
 
 def resetIT() {
+    changedFile = new File(basedir, "src/main/java/first/First.java")
+    VerifyUtil.replaceAllInFileStatic(changedFile, "Set g", "Set<Integer> g")
+
     verifyUtil.deleteFile(firstRun)
     verifyUtil.deleteFile(new File(basedir, ".starts/deps.zlc"))
     verifyUtil.deleteFolder(new File(basedir, "target"))
