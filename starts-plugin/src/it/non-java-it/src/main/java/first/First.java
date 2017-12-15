0ff5a5db6c35d877ee53d8790857ac2ef7f78b5d
Jeff Hong <hong66@illinois.edu>
2017-12-15 11:22:11 -0600

    rebased from IT-clone-fix, removes unnecessary code, follows good coding style

--- starts-plugin/src/it/non-java-it/src/main/java/first/First.java
+++ starts-plugin/src/it/non-java-it/src/main/java/first/First.java
@@ -3,43 +3,26 @@ package first;
 import java.io.File;
 import java.net.URL;
 import java.nio.file.Paths;
-import java.util.LinkedHashSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class First {
-    private LinkedHashSet output;
-
-    public First() {
-        super();
-        output = new LinkedHashSet();
+    public First() { }
+    public String readXML() {
+        String fullFileName = "";
+        StringBuilder result = new StringBuilder();
+        try {
+            URL xmlResource = this.getClass().getClassLoader().getResource("books.xml");
+            fullFileName = Paths.get(xmlResource.toURI()).toRealPath().toString();
+            File file = new File(fullFileName);
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                result.append(line).append("\n");
+            }
+            scanner.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return result.toString();
     }
-
-    public void add(int addend) {
-        output.add(addend);
-    }
-
-    public Set<Integer> getSet() {
-        return output;
-    }
-	
-	public String readXML() {
-    	String fullFileName = "";
-		StringBuilder result = new StringBuilder();
-		try {
-			URL resource = this.getClass().getClassLoader().getResource("books.xml");
-			fullFileName = Paths.get(resource.toURI()).toRealPath().toString();
-			File file = new File(fullFileName);
-			Scanner scanner = new Scanner(file);
-			while (scanner.hasNextLine()) {
-				String line = scanner.nextLine();
-				result.append(line).append("\n");
-			}
-		
-			scanner.close();
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-		return result.toString();
-	}
 }
--- starts-plugin/src/it/non-java-it/verify.groovy
+++ starts-plugin/src/it/non-java-it/verify.groovy
@@ -2,20 +2,24 @@
  * Copyright (c) 2015 - Present. The STARTS Team. All Rights Reserved.
  */
 
-import edu.illinois.starts.jdeps.VerifyUtil;
+import edu.illinois.starts.jdeps.VerifyUtil
 
-targetDir = new File(basedir, "target");
-firstRun = new File(basedir, "first-run.txt");
-verifyUtil = new VerifyUtil(new File(basedir, "build.log"));
+firstRun = new File(basedir, "first-run.txt")
+verifyUtil = new VerifyUtil(new File(basedir, "build.log"))
 
 if (!firstRun.exists()) {
-    firstRun.createNewFile();
-	verifyUtil.assertCorrectlyAffected("1");
+    verifyUtil.assertCorrectlyAffected("1")
+    firstRun.createNewFile()
 } else {
-    verifyUtil.deleteFile(firstRun);
-    verifyUtil.deleteFile(new File(basedir, ".starts/deps.zlc"));
-    file = new File(basedir, "src/main/resources/books.xml");
-    VerifyUtil.replaceAllInFileStatic(file, "boook", "bk");
-    verifyUtil.deleteFolder(targetDir);
-	verifyUtil.assertCorrectlyAffected("0");
+    verifyUtil.assertCorrectlyAffected("0")
+    resetIT()
+}
+
+def resetIT() {
+    file = new File(basedir, "src/main/resources/books.xml")
+    VerifyUtil.replaceAllInFileStatic(file, "boook", "bk")
+
+    verifyUtil.deleteFile(firstRun)
+    verifyUtil.deleteFile(new File(basedir, ".starts/deps.zlc"))
+    verifyUtil.deleteFolder(new File(basedir, "target"))
 }
