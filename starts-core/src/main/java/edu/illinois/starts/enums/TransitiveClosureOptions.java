b94f69385516259d4a4096437977dc84657b509e
Valeria Marques <vmm49@cornell.edu>
2022-10-11 20:31:06 -0400

    More explanation on transitive closure options

--- starts-core/src/main/java/edu/illinois/starts/enums/TransitiveClosureOptions.java
+++ starts-core/src/main/java/edu/illinois/starts/enums/TransitiveClosureOptions.java
@@ -2,6 +2,20 @@ package edu.illinois.starts.enums;
 
 /**
  * Options for computing transitive closure.
+ * Consider the example graph presented in "Techniques for Evolution-Aware Runtime Verification"
+ * (https://www.cs.cornell.edu/~legunsen/pubs/LegunsenETAL19eMOP.pdf):
+ *     A  --> []
+ *     B  --> [A]
+ *     C  --> [B, D]
+ *     D  --> []
+ *     E  --> [D]
+ *     TC --> [C]
+ *     TE --> [E]
+ * The options will return the following values when performed on B:
+ *     TRANSITIVE --> [B, C, TC]
+ *     TRANSITIVE_AND_INVERSE_TRANSITIVE --> [A, B, C, TC]
+ *     TRANSITIVE_OF_INVERSE_TRANSITIVE --> [A, B, C, D, TC]
+ * These options correspond to ps3, ps2, and ps1, respectively, in the aforementioned paper
  */
 public enum TransitiveClosureOptions {
     TRANSITIVE,
